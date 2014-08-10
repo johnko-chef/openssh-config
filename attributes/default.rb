20b128db34d65bfd9f54b60a6c0903e40003bb46
John Ko <git@johnko.ca>
2014-08-10 07:27:13 -0400

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -91,9 +91,11 @@ when 'freebsd'
   override['openssh']['server']['client_alive_interval'] = '60'
   override['openssh']['server']['compression'] = 'no'
   override['openssh']['server']['hostKey'] = [ '/etc/ssh/ssh_host_ecdsa_key', '/etc/ssh/ssh_host_rsa_key' ]
-  override['openssh']['server']['kex_algorithms'] = 'ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256'
+  #TODO remove sha1 when fabric supports sha2
+  override['openssh']['server']['kex_algorithms'] = 'ecdh-sha2-nistp256,ecdh-sha2-nistp384,ecdh-sha2-nistp521,diffie-hellman-group-exchange-sha256,diffie-hellman-group-exchange-sha1,diffie-hellman-group14-sha1,diffie-hellman-group1-sha1'
   override['openssh']['server']['listen_address'] = node['ipaddress']
   override['openssh']['server']['login_grace_time'] = '30s'
+  #TODO remove sha1 when fabric supports sha2
   override['openssh']['server']['m_a_cs'] = 'umac-64-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-256-etm@openssh.com,hmac-sha2-512-etm@openssh.com,umac-64@openssh.com,umac-128@openssh.com,hmac-sha2-256,hmac-sha2-512,hmac-sha1'
   override['openssh']['server']['max_auth_tries'] = '3'
   override['openssh']['server']['permit_root_login'] = 'yes'
